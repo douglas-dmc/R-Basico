c130340f31eb14e05905fc4d666c6a97e08babd6
douglas-dmc <douglasmarques37@gmail.com>
2025-01-13 18:38:28 -0300

    feat: melhorando o código para imprimir um gráfico de barras horizontal dos ativos

--- portfolio.R
+++ portfolio.R
@@ -30,8 +30,30 @@ preco_medio <- round(preco_total_soma/nr_cotas_soma, 2)
 # Criando o dataframe
 df_portfolio <- data.frame(preco_total_soma, nr_cotas_soma, preco_medio)
 
-# Alterando os nomes das colunas
-colnames(df_portfolio) <- c("Aporte", "Nr de Cotas", "Preco Medio")
-
 # Removendo linhas com 0 na coluna 'Nr de Cotas'
-df_portfolio <- df_portfolio[df_portfolio$`Nr de Cotas` != 0, ]
+df_portfolio <- df_portfolio[df_portfolio$`nr_cotas_soma` != 0, ]
+
+# Ordenando df_portfolio pelo 'Aporte'
+df_portfolio <- df_portfolio[order(df_portfolio$preco_total_soma, 
+                                   decreasing = TRUE), ]
+
+# Inserindo a coluna de ativos
+df_portfolio <- cbind(df_portfolio, dimnames(df_portfolio)[1])
+
+# Alterando os nomes das colunas
+colnames(df_portfolio) <- c("Aporte", "Nr de Cotas", "Preco Medio", "Ativo")
+
+# Criando um gráfico de barras horizontal
+library(ggplot2)
+library(forcats)
+
+ggplot(df_portfolio, aes(y = Aporte, x = fct_rev(fct_inorder(Ativo)), 
+                         fill = Ativo)) +
+    geom_bar(stat = "identity", width = .75, show.legend = F) + 
+    coord_flip() +
+    labs(
+        x = "Ticker (FII e FI-INFRA",
+        y = "Custo de Aquisição (R$)",
+        color = "Aporte (R$)",
+        title = "Gráfico de Custo de Aquisição por Ativo"
+    )
