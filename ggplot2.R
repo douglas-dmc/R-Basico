bd567db51e363b5f99dd43eaab06525ab2c1e097
douglas.dmc <douglasmarques37@gmail.com>
2025-01-17 11:56:15 -0300

    feat(ggplot2): carregando e entendendo o dataset 'mpg'.

--- ggplot2.R
+++ ggplot2.R
@@ -14,6 +14,24 @@ data(mpg)
 # Visualizando a estrutura do mpg
 str(mpg)
 
+# Entendendo o dataframe mpg
+?mpg
+
+# manufactorer : fabricante
+# displ        : capacidade do motor em litros
+# year         : ano de fabricação
+# cyl          : numero de cilindros
+# trans        : tipo de transmissão
+# drv          : tipo de trem de força, onde f = tração dianteira, 
+#                                            r = tração traseira, 4 = 4x4
+# hwy          : autonomia na estrada (milhas por galão)
+# fl           : tipo de combutivel
+# class        : tipo de carro          
+
 # Plotando gráfico de pontos (displ x hwy)
 ggplot(data = mpg) +
-    geom_point(mapping = aes(x = displ, y = hwy))
+    geom_point(mapping = aes(x = displ, y = hwy, color = class))
+
+# Plotando gráfico de dispersão (hwy x cyl)
+ggplot(data = mpg) +
+    geom_point(mapping = aes(x = hwy, y = cyl))
