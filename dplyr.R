f9820c73c54e6ae643210d25160c8e68e23dc8fa
douglas-dmc <douglasmarques37@gmail.com>
2025-01-20 10:32:27 -0300

    feat(dplyr): inserindo exemplos das funções select() e rename().

--- dplyr.R
+++ dplyr.R
@@ -70,3 +70,79 @@ fun_arrange(flights, carrier)
 
 # Usando funções de 'data-masking'
 arrange(pick(month, day))
+
+# Função select()
+
+# Selecionando as colunas por índices
+select(flights, 2:5)
+
+# selecionando colunas pelo nome
+select(flights, year, month, day)
+
+# Selecionando intervalo de colunas
+select(flights, year:dep_delay)
+
+# Selecionando todas as colunas exceto as especificadas
+select(flights, -(year:dep_delay))
+
+# Selecionando colunas com a função starts_with()
+select(flights, starts_with("dep"))
+
+select(flights, starts_with(c("dep", "arr")))
+
+# Selecionando colunas com a função ends_with()
+select(flights, ends_with("time"))
+
+select(flights, ends_with(c("time", "delay")))
+
+# Selecionando as colunas que não terminam com 'time' e nem com 'delay'
+select(flights, !ends_with("time") & !ends_with("delay"))
+
+# Selecionando a última coluna do dataframe
+select(flights, last_col())
+
+# Selecionando colunas com a função contains()
+select(flights, contains("de"))
+
+# Selecionando colunas com a função matches()
+select(flights, matches("(.)\\1")) # utiliza uma expressão regular
+
+# Reposicionando colunas para o início do dataframe
+select(flights, time_hour, air_time, everything())
+
+# Selecionando todas as colunas numéricas
+select_if(flights, is.numeric)
+
+# Selecionando colunas com a função where()
+select(flights, where(is.numeric))
+select(flights, where(~ is.numeric(.x))) # resultado igual ao código anterior
+
+# Selecionando colunas numéricas com média superior a um valor
+select(flights, where(~ is.numeric(.x) && mean(.x, na.rm = TRUE) > 400))
+
+# Selecionando colunas com vetores: all_of() e any_of()
+vars <- c("dep_time", "dep_delay", "arr_time", "arr_delay")
+
+select(flights, all_of(vars)) # seleciona todas as varisveis do vetor. 
+# Se o vetor tiver alguma variavel não correspondente a colunas do dataframe
+# haverá um erro
+
+vars <- c("dep_time", "dep_delay", "arr_time", "arr_delay", "source")
+
+select(flights, any_of(vars)) # seleciona, somente, as colunas que correspondem
+# às variáveis do vetor
+
+# Função rename()
+
+# Renomeando colunas
+rename(flights, tail_num = tailnum)
+
+# Renomeando com as funções predicadas
+nams <- c(tail_num = "tailnum", destine = "dest")
+
+rename(flights, all_of(nams))
+
+# Renomeando com a função rename_with()
+rename_with(flights, toupper, month)
+
+rename_with(flights, tolower, month)
